99177ac94f522673b70d7a2e93c552636c1ff4ed
Crystal Michelle Boswell <cmboswell458@mail.cfcc.edu>
2026-04-19 21:38:31 -0400

    Final database project submission

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Database Project</title>
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+
+    <h1>Student Database Project</h1>
+
+    <p>Name: Crystal Michelle Boswell</p>
+
+    <p>Date: <?php echo date("Y-m-d"); ?></p>
+
+</body>
+</html>
